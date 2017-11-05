ac3906dc5e9021957dfa8ce9e68a21929bcd3352
Joey Cho <cjy12182000@naver.com>
2017-11-05 18:58:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function printBadges(array){
+  for (var i = 0; i < array.length; i++) {
+    console.log(array[i])
+    print(`Welcome ${array[i]}! You are employee #${i+1}.`)
+  }
+  return array
+}
