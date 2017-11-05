0edbfd208e021e65224418425fae7f96455a77a5
Joey Cho <cjy12182000@naver.com>
2017-11-05 19:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function printBadges(array){
-  for (var i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     console.log(array[i])
     print(`Welcome ${array[i]}! You are employee #${i+1}.`)
   }
